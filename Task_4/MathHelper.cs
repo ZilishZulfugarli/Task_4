a6e64b2f3daa2b7d54bce56f0a9428e3b02f1f7c
ZilishZulfugarli <2002zilis@gmail.com>
2023-05-30 23:09:54 +0400

    Add project files.

---
+++ Task_4/MathHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    internal class MathHelper
+    {
+        public  int PrintValue(int a , int b)
+        {
+            return a + b;
+        }
+
+        public bool PrintValue(bool a)
+        {
+            
+            if (true)
+            {
+                Console.WriteLine(1);
+            }
+            if (false)
+            {
+                Console.WriteLine(0);
+            }
+            return a ;
+        }
+
+        public string PrintValue(string a, string b) 
+        {
+            return a + b;
+        }
+        
+        public int PrintValue(int a , int b , int c)
+        {
+            return a * b * c;
+        }
+
+    }
+}
---
+++ Task_4/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathHelper m= new MathHelper();
+
+            int num1 = m.PrintValue(10 , 20);
+            Console.WriteLine(num1);
+
+            bool b1 = m.PrintValue(false);
+            Console.WriteLine(b1);
+
+            string s = m.PrintValue("Hello", " World");
+            Console.WriteLine(s);
+
+            int mul = m.PrintValue(10, 20, 30);
+            Console.WriteLine(mul);
+
+        }
+
+        
+    }
+}
